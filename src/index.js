58bc4b4d3f4cb3e00e341c267ee40dba20f793fe
DarisIlham <darisilham32@gmail.com>
2025-09-15 10:59:00 +0700

    menambahkan app get di index

--- src/index.js
+++ src/index.js
@@ -14,3 +14,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
 console.log(`Server running on port ${port}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Backend ppb is running on Vercel");
+});
