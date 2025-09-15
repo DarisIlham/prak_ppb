d6497584f57077eb032bbd99f0d551dc8906dcc3
DarisIlham <darisilham32@gmail.com>
2025-09-15 11:35:04 +0700

    sakdkaskdsa

--- src/routes/medicationRoutes.js
+++ src/routes/medicationRoutes.js
@@ -3,9 +3,10 @@ import { MedicationController } from
 "../controllers/medicationController.js";
 const router = express.Router();
 router.get("/", MedicationController.getAll);
+router.get("/medications/search", MedicationController.search);
 router.get("/:id", MedicationController.getById);
 router.post("/", MedicationController.create);
 router.put("/:id", MedicationController.update);
 router.delete("/:id", MedicationController.remove);
-router.get("/medications/search", MedicationController.search);
+
 export default router;
